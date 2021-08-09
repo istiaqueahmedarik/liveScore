73c5894edd6081a4fd66f4190e22a04c8d3cbe4a
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2021-08-10 00:51:04 +0600

    fg

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ module.exports = {
   reactStrictMode: true,
 }
 module.exports = {
-  future: {
-    webpack5: true,
-  },
+  
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
